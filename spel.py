ab77016b9a6e3b9849b6ab1e868b303f46e32642
davmyr <31848515+davmyr@users.noreply.github.com>
2021-02-22 15:11:10 +0100

    Add files via upload

---
+++ spel.py
@@ -0,0 +1,12 @@
+import time
+import random
+## 1 greet player -> short background story
+## 2 prompt to opt in to play
+## 3 first part of story and make a choice 
+
+## theme: explore island, run into monsters/scary animals. survive
+## find food, water and shelter.
+## find a weapon and tools to make a fire and build shelter
+## goal of game - getting of the island. by helicopter, build a raft or by boat. or die
+
+##You wake up washed ashore on a desert island, with no recollection of how you ended up here. 
